0c1f9f3a50c297344cbf6f5c5d86b7752322932b
War-Oxi <rlaxowl5460@gmail.com>
2021-11-02 10:18:59 +0900

    11-02

--- JAVA/Test/src/Test1.java
+++ JAVA/Test/src/Test1.java
@@ -3,12 +3,25 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String args[]){
-        Scanner Plus = new Scanner(System.in);
-        int Num = Plus.nextInt();
-        int Result = 0;
-        for (int i = Num; i<=100; i+=Num){
-            Result += i;
+        Scanner stdin = new Scanner(System.in);
+        int i;
+        double sum=0.0, avg;
+        double dnum[] = new double[5];
+        System.out.println("drum 배열으 길이 : " + dnum.length);
+        System.out.print("초기화 하지 않은 dnum[]의 값: ");
+        for(i=0; i<dnum.length; i++){
+            System.out.print("dnum["+i+"] 번째 데이터 입력 : ");
+            dnum[i] = stdin.nextDouble();
         }
-        System.out.println(Result);
+        System.out.println("배열의 내용");
+        for(i=0; i<dnum.length; i++){
+            System.out.print(dnum[i]+" ");
+        }
+        System.out.println();
+        for(i=0; i<dnum.length; i++)
+            sum = sum + dnum[i];
+        System.out.println("배열의 합은 "+sum+ " 입니다.");
+        avg = sum/dnum.length;
+        System.out.println("배열의 평균 값은 " + avg + "입니다.");
     }
 }
