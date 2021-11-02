7966e747793fcbddf459f7cb41e7ce193ad6a23d
War-Oxi <xowl5460@naver.com>
2021-11-02 11:35:08 +0900

    11-02 desktop

--- JAVA/Test/src/Main.java
+++ JAVA/Test/src/Main.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main{ 
     public static void main(String args[]){
-    	int i = 1;
-        for(i=1 ; i<9; i=i+2){
-            for(int j=1; j<9-i; j=j+2)
-                System.out.print(" ");
-            for(int k=1; k<=i; k++)
-                System.out.print("*");
-            System.out.println();
+        Scanner stdin = new Scanner(System.in);
+    	List<Integer> list_num = new ArrayList<>();
+        System.out.print("정수들을 입력 > ");
+        
+        int i=5;
+        while(true){
+            i = stdin.nextInt();
+            if (i<0)
+                break;
+            list_num.add(i);
         }
-        while(i>=1){
-            for(int j=9; j>i; j=j-2)
-                System.out.print(" ");
-            for(int k=1; k<=i; k++)
-                System.out.print("*");
-            i=i-2;
-            System.out.println();
+        
+        System.out.print("검색할 배수 입력 > ");
+        int mul = stdin.nextInt();
+        iteration:{
+            for(int j : list_num){
+                if(j%mul==0)
+                System.out.print(j+" ");
+            }
         }
     }
 }
