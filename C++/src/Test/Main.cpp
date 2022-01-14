c11472f8e7529c0e673313c84baaa650a6b32bcd
War-Oxi <rlaxowl5460@gmail.com>
2022-01-15 05:47:49 +0900

    new & delete

--- C++/src/Test/Main.cpp
+++ C++/src/Test/Main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int& RefRetFuncOne(int &ref){
-    ref++;
-    return ref;
+typedef struct __Point{
+    int xpos;
+    int ypos;
+} Point;
+
+Point& PntAdder(const Point &p1, const Point &p2){
+    Point *pt = new Point;
+    pt->xpos = p1.xpos + p2.xpos;
+    pt->ypos = p1.ypos + p2.ypos;
+    return *pt;
 }
 
 int main(void){
-    int num1=1;
-    int num2=RefRetFuncOne(num1);
+    Point *p1 = new Point;
+    Point *p2 = new Point;
+    p1->xpos = 2;
+    p1->ypos = 4;
 
-    num1++;
-    num2++;
-    cout<<"num1: "<<num1<<endl;
-    cout<<"num2: "<<num2<<endl;
+    p2->xpos = 12;
+    p2->ypos = 14;
+
+    Point *p3 = &PntAdder(*p1, *p2);
+    cout<<p3->xpos<<','<<p3->ypos;
+    
+    delete p1;
+    delete p2;
+    delete p3;
 
     return 0;
 }
