1cf24b092d25791f1b379d1c2f7cddf3b669c9ac
War-Oxi <xowl5460@naver.com>
2022-01-16 05:24:32 +0900

    03-1

--- C++/src/Test/Main.cpp
+++ C++/src/Test/Main.cpp
@@ -1,62 +1,39 @@
 #include <iostream>
 using namespace std;
 
-#define ID_LEN 20
-#define MAX_SPD 200
-#define FUEL_STEP 2
-#define ACC_STEP 10
-#define BRK_STEP 10
-
-struct Car
+struct Point
 {
-	char gamerID[ID_LEN];	// 소유자ID
-	int fuelGauge;		// 연료량
-	int curSpeed;		// 현재속도
+    int xpos;
+    int ypos;
+
+    void MovePos(int x, int y)
+    {
+        xpos += x;
+        ypos += y;
+    }
+
+    void AddPoint(const Point &pos)
+    {
+        xpos += pos.xpos;
+        ypos += pos.ypos;
+    }
+
+    void ShowPosition()
+    {
+        cout << "[" << xpos << ", " << ypos << "]" << endl;
+    }
 };
 
-void ShowCarState(const Car &car)
+int main(void)
 {
-    cout<<"소유자ID: "<<car.gamerID<<endl;
-    cout<<"연료량: "<<car.fuelGauge<<"%"<<endl;
-    cout<<"현재속도: "<<car.curSpeed<<"km/s"<<endl<<endl;
-}
+    Point pos1 = {12, 4};
+    Point pos2 = {20, 30};
 
-void Accel(Car &car)
-{
-    if(car.fuelGauge<=0)
-        return;
-    else
-        car.fuelGauge-=FUEL_STEP;
+    pos1.MovePos(-7, 10);
+    pos1.ShowPosition();
 
-    if(car.curSpeed+ACC_STEP>=MAX_SPD)
-    {
-        car.curSpeed=MAX_SPD;
-        return;
-    }
-
-    car.curSpeed+=ACC_STEP;
-}
-void Break(Car &car)
-{
-    if(car.curSpeed<BRK_STEP)
-    {
-        car.curSpeed=0;
-        return;
-    }
-
-    car.curSpeed-=BRK_STEP;
-}
-
-int main(void){
-    Car run99={"run99", 100, 0};
-
-    ShowCarState(run99);
-    Accel(run99);
-    Accel(run99);
-    ShowCarState(run99);
-    Break(run99);
-    ShowCarState(run99);
-    Break(run99);
+    pos1.AddPoint(pos2);
+    pos1.ShowPosition();
 
     return 0;
 }
