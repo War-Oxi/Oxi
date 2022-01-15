ec5a2b674a432b936595a6640eaeb43b191a6e22
War-Oxi <xowl5460@naver.com>
2022-01-16 05:04:03 +0900

    RacinCar.cpp

--- C++/src/Test/Main.cpp
+++ C++/src/Test/Main.cpp
@@ -49,13 +49,14 @@ void Break(Car &car)
 
 int main(void){
     Car run99={"run99", 100, 0};
-    
+
     ShowCarState(run99);
     Accel(run99);
     Accel(run99);
     ShowCarState(run99);
     Break(run99);
     ShowCarState(run99);
+    Break(run99);
 
     return 0;
 }
