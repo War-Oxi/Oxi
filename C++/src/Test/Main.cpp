f9d0517f60e1d985edb0ac2be7ae345bc6d8630e
War-Oxi <xowl5460@naver.com>
2022-01-16 15:57:07 +0900

    RacingCarEnum

--- C++/src/Test/Main.cpp
+++ C++/src/Test/Main.cpp
@@ -1,39 +1,69 @@
 #include <iostream>
 using namespace std;
 
-struct Point
+namespace CAR_CONST
 {
-    int xpos;
-    int ypos;
+	enum
+	{
+		ID_LEN		=20,
+		MAX_SPD		=200,
+		FUEL_STEP	=2,
+		ACC_STEP	=10,
+		BRK_STEP	=10
+	};
+}
 
-    void MovePos(int x, int y)
-    {
-        xpos += x;
-        ypos += y;
-    }
+struct Car
+{
+	char gamerID[CAR_CONST::ID_LEN];	
+	int fuelGauge;		
+	int curSpeed;		
 
-    void AddPoint(const Point &pos)
-    {
-        xpos += pos.xpos;
-        ypos += pos.ypos;
-    }
+	void ShowCarState()
+	{
+		cout<<"소유자ID: "<<gamerID<<endl;
+		cout<<"연료량: "<<fuelGauge<<"%"<<endl;
+		cout<<"현재속도: "<<curSpeed<<"km/s"<<endl<<endl;
+	}
+	void Accel()
+	{
+		if(fuelGauge<=0)
+			return;
+		else
+			fuelGauge-=CAR_CONST::FUEL_STEP;
 
-    void ShowPosition()
-    {
-        cout << "[" << xpos << ", " << ypos << "]" << endl;
-    }
+		if((curSpeed+CAR_CONST::ACC_STEP)>=CAR_CONST::MAX_SPD)
+		{
+			curSpeed=CAR_CONST::MAX_SPD;
+			return;
+		}
+	
+		curSpeed+=CAR_CONST::ACC_STEP;
+	}
+	void Break()
+	{
+		if(curSpeed<CAR_CONST::BRK_STEP)
+		{
+			curSpeed=0;
+			return;
+		}
+
+		curSpeed-=CAR_CONST::BRK_STEP;
+	}
 };
 
 int main(void)
 {
-    Point pos1 = {12, 4};
-    Point pos2 = {20, 30};
+	Car run99={"run99", 100, 0};
+	run99.Accel();
+	run99.Accel();
+	run99.ShowCarState();
+	run99.Break();
+	run99.ShowCarState();
 
-    pos1.MovePos(-7, 10);
-    pos1.ShowPosition();
-
-    pos1.AddPoint(pos2);
-    pos1.ShowPosition();
-
-    return 0;
+	Car sped77={"sped77", 100, 0};
+	sped77.Accel();
+	sped77.Break();
+	sped77.ShowCarState();
+	return 0;
 }
