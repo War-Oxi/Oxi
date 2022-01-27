d2945845fc7767015dd57c422d997b9f07c02512
War-Oxi <xowl5460@naver.com>
2022-01-28 05:46:14 +0900

    Printer

--- C++/src/Test/Main.cpp
+++ C++/src/Test/Main.cpp
@@ -1,62 +1,29 @@
 #include <iostream>
 using namespace std;
 
-class Calculator{
+class Printer{
 private:
-	int numOfAdd;
-	int numOfMin;
-	int numOfMul;
-	int numOfDiv;
-public :
-	void Init();
-	double Add(double num1, double num2);
-	double Min(double num1, double num2);
-	double Mul(double num1, double num2);
-	double Div(double num1, double num2);
-	void ShowOpCount();
+	char str[30];
+public:
+	void SetString(char* s);
+	void ShowString();
 };
 
-void Calculator::Init(){
-	numOfAdd=0;
-	numOfMin=0;
-	numOfMul=0;
-	numOfDiv=0;
+void Printer::SetString(char* s){
+	strcpy(str, s);
 }
 
-double Calculator::Add(double num1, double num2){
-	numOfAdd++;
-	return num1 + num2;
-}
-
-double Calculator::Min(double num1, double num2){
-	numOfMin++;
-	return num1 - num2;
-}
-
-double Calculator::Mul(double num1, double num2){
-	numOfMul++;
-	return num1 * num2;
-}
-
-double Calculator::Div(double num1, double num2){
-	numOfDiv++;
-	return num1 / num2;
-}
-
-void Calculator::ShowOpCount(){
-	cout<<"덧셈 : "<<numOfAdd<<endl;
-	cout<<"뺄셈 : "<<numOfMin<<endl;
-	cout<<"곱셈 : "<<numOfMul<<endl;
-	cout<<"나눗셈 : "<<numOfDiv<<endl;
+void Printer::ShowString(){
+	cout<<str<<endl;
 }
 
 int main(void){
-	Calculator cal;
-	cal.Init();
-	cout<<"3.2 + 2.4 = "<<cal.Add(3.2, 2.4)<<endl;
-	cout<<"3.5 / 1.7 = "<<cal.Div(3.5, 1.7)<<endl;
-	cout<<"2.2 - 1.5 = "<<cal.Min(2.2, 1.5)<<endl;
-	cout<<"4.9 / 1.2 = "<<cal.Div(4.9, 1.2)<<endl;
-	cal.ShowOpCount();
+	Printer pnt;
+	pnt.SetString("Hello World!");
+	pnt.ShowString();
+
+
+	pnt.SetString("I love C++");
+	pnt.ShowString();
 	return 0;
 }
