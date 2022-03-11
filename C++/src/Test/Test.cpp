e4d09996f4c61490d0b6a439c2464f5cfd729afe
War-Oxi <xowl5460@naver.com>
2022-03-11 16:00:46 +0900

    22-03-11

--- C++/src/Test/Test.cpp
+++ C++/src/Test/Test.cpp
@@ -1,20 +1,36 @@
-#include <iostream>
-#include <cstring>
+#include "iostream"
 using namespace std;
 
-char * MakeStrAdr(int len)
-{
-    // char * str=(char*)malloc(sizeof(char)*len);
-    char * str=new char[len];
-    return str;
-}
+class SimpleClass{
+private:
+    int num1;
+    int num2;
+public:
+    SimpleClass(){
+        num1=0;
+        num2=0;
+    }
+    SimpleClass(int n){
+        num1=n;
+        num2=0;
+    }
+    SimpleClass(int n1, int n2){
+        num1=n1;
+        num2=n2;
+    }
+    void ShowData() const{
+        cout<<num1<<' '<<num2<<endl;
+    }
+};
 
-int main()
-{
-    char * str=MakeStrAdr(20);
-    strcpy(str, "I am so happy~");
-    cout<<str<<endl;
-    // free(str);
-    delete []str;
+int main(){
+    SimpleClass sc1;
+    sc1.ShowData();
+
+    SimpleClass sc2(100);
+    sc2.ShowData();
+
+    SimpleClass sc3(100, 200);
+    sc3.ShowData();
     return 0;
 }
