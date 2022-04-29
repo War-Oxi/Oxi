22a308aab460a8974871273f7c3eaa8ea8acd507
War-Oxi <xowl5460@naver.com>
2022-04-30 02:08:40 +0900

    DESK TOP

--- Python_Code/Python Code/test.py
+++
@@ -1,3 +0,0 @@
-a = {'name' = 'hong'}
-
-print(a)
---
+++ Python_Code/pythonProject/main.py
@@ -0,0 +1,10 @@
+# for i in range(1,6,1):
+#     print("#" * i)
+
+# for i in range(5,0,-1):
+#     print("#" * i)
+
+for i in range(0, 5, 1):
+    print(' '*(4-i)+"#"*(i*2+1))
+for i in range(4,-1,-1):
+    print(' '*(4-i)+"#"*(i*2+1))
