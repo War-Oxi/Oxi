160e5eda33f44b495476aa7036a379020a26954c
War-Oxi <xowl5460@naver.com>
2022-06-06 18:30:12 +0900

    MAC

--- Python_Code/University_real/DataScience/data_assignment.py
+++ Python_Code/University_real/DataScience/data_assignment.py
@@ -16,8 +16,6 @@ def rating(avg):
 data2 = data.copy()
 data2['평균'] = (data['국어'] + data['영어'] + data['수학']) / 3
 data2['등급'] = "R"
-print(data2)
 for i in range(0, 20):
     data2['등급'][i] = rating(data2['평균'][i])
 print(data2)
-# print(data)
