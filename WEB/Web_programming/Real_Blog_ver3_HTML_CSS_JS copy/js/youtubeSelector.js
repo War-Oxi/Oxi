ed3087436ba5e45f2200fb6ee2592047302f7bdd
War-Oxi <xowl5460@naver.com>
2022-06-14 13:41:51 +0900

    MAC

---
+++ WEB/Web_programming/Real_Blog_ver3_HTML_CSS_JS copy/js/youtubeSelector.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var Width = 800;
+  var Height = 500;
+  $("this").clock(function(){
+    $(this).width(xWidth).addClass("select");
+    $(this).hegith(Height).addClass("select");
+  })
+})
