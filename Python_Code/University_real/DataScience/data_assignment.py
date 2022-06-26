56f8350cb25986a9f0dfa9f17976615a52678fae
War-Oxi <xowl5460@naver.com>
2022-06-27 01:36:29 +0900

    MAC

--- Python_Code/University_real/DataScience/data_assignment.py
+++ Python_Code/University_real/DataScience/data_assignment.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pandas as pd
 
 data = pd.read_csv('sample.csv')
@@ -19,3 +20,24 @@ data2['등급'] = "R"
 for i in range(0, 20):
     data2['등급'][i] = rating(data2['평균'][i])
 print(data2)
+=======
+import pandas as pd
+
+data = pd.read_csv('sample.csv')
+def Func(row):
+    if(row > 90):
+        return 'A'
+    elif(row>80):
+        return 'B'
+    else:
+        return 'C'
+
+data['총합'] = data['국어']+data['영어']+data['수학']
+data['평균'] = data['총합']/3
+data['학점'] = data['평균'].apply(Func)
+data['등수'] = data['평균'].rank()
+data = data.sort_values('등수')
+data = data.set_index('등수')
+
+print(data)
+>>>>>>> Stashed changes
--- Python_Code/University_real/pydata/fig5.py
+++ Python_Code/University_real/pydata/fig5.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,3 +20,28 @@ data.plot(kind='bar', y=columns, ax=ax)
 
 plt.show()
 
+=======
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('sample.csv')
+
+plt.figure(num="성적 그래프")
+
+plt.rcParams.update({'font.family':'AppleGothic', 'font.size':12})
+plt.title("학번별 성적")
+
+
+ax = plt.gca()
+
+columns = ['국어', '영어', '수학']
+
+data.plot(kind='line', y=columns, ax=ax, marker='o')
+data.plot(kind='bar', y=columns, ax=ax)
+
+xlabel = ("학번")
+ylabel = ("성적")
+plt.show()
+
+
+>>>>>>> Stashed changes
