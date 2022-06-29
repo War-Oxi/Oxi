5ac01e055fd6796789eb20588cb6244940220a2b
War-Oxi <xowl5460@naver.com>
2022-06-30 04:17:27 +0900

    MAC

---
+++ C++/src/TBC/add.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 김태지 on 2022/06/29.
+//
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ C++/src/TBC/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include <algorithm>
+using namespace std;
+
+//#define LIKE_APPLE
+
+int main()
+{
+    int i = 123;    //copy initialization
+    int a(123);     //direct initialization
+    int b{123};     //uniform initialization
+
+    /**
+     * 변수 선언은 언제 하는 것이 좋은가?
+     * 최근에는 사용할 변수를 사용하기 직전에 선언하는 것을 추천한다.
+     */
+    return 0;
+}
