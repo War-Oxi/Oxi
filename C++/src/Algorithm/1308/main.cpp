d0b31d709e9b9afc3626398cdccafdfe358e39ab
War-Oxi <xowl5460@naver.com>
2022-07-04 18:32:52 +0900

    MAC

--- C++/src/Algorithm/1308/main.cpp
+++ C++/src/Algorithm/1308/main.cpp
@@ -26,10 +26,7 @@ int main()
 {
     int start_year(0), start_month(0), start_day(0);
     int end_year(0), end_month(0), end_day(0);
-//    int start_year(2008), start_month(12), start_day(27);
-//    int end_year(2009), end_month(1), end_day(22);
-//    int start_year(2024), start_month(2), start_day(15);
-//    int end_year(2024), end_month(6), end_day(17);
+
     cin >> start_year >> start_month >> start_day;
     cin >> end_year >> end_month >> end_day;
 
@@ -37,28 +34,23 @@ int main()
     int diff_month(end_month - start_month);
     int diff_day(end_day - start_day);
 
-    if(diff_year < 1000 || (diff_year == 1000 && diff_month < 0) || (diff_year == 1000 && diff_month == 0 && diff_day < 0))
-    {
+    if(diff_year < 1000 || (diff_year == 1000 && diff_month < 0) || (diff_year == 1000 && diff_month == 0 && diff_day < 0)) {
         cout << "D-" << YearCalculator(start_year, start_month, start_day, end_year, end_month, end_day) << endl;
     }
-    else
-    {
+    else {
         cout << "gg" << endl;
     }
 
     return 0;
 }
 
-int YearCalculator(int start_year, int start_month, int start_day, int end_year, int end_month, int end_day)
-{
+int YearCalculator(int start_year, int start_month, int start_day, int end_year, int end_month, int end_day) {
     int count(0);
 
-    if (start_day>end_day)
-    {
+    if (start_day > end_day) {
         count += g_month[start_month - 1] - start_day;
         count += end_day;
-    }
-    else //start_day<=end_day
+    } else //start_day<=end_day
     {
         count += end_day - start_day;
     }
@@ -68,126 +60,73 @@ int YearCalculator(int start_year, int start_month, int start_day, int end_year,
      * 년도가 다르고 start_month가 end_month보다 작을 때
      * 년도가 같을 때
      */
-    if(start_year != end_year)
-    {
-        /**
-          * 5. 윤년 계산하기
-          *  5.1 윤년일 떄
-          *  5.2 윤년일 때 (start_month가 2보다 클 때)
-          *  5.3 윤년일 떄 (end_month가 2보다 작을 때)
-          */
-        for(int i(start_year); i < end_year; i++)
-        {
-            if (CheckLeapYear(i))
-            {
+    if (start_year != end_year) {
+        for (int i(start_year); i < end_year; i++) {
+            if (CheckLeapYear(i)) {
                 count++;
             }
         }
-        if ((CheckLeapYear(start_year) && start_month > 2) && (CheckLeapYear(end_year) && end_month < 2))
-        {
+        if ((CheckLeapYear(start_year) && start_month > 2) && (CheckLeapYear(end_year) && end_month < 2)) {
             count -= 2;
-        }
-        else if ((CheckLeapYear(start_year) && start_month > 2) || (CheckLeapYear(end_year) && end_month < 2))
-        {
+        } else if ((CheckLeapYear(start_year) && start_month > 2) || (CheckLeapYear(end_year) && end_month < 2)) {
             count -= 1;
         }
-
-        if (start_month < end_month)
-        {
-            for(; start_year < end_year; start_year++)
-            {
-                count+= 365;
+        //년도, 월 차이
+        if (start_month < end_month) {
+            for (; start_year < end_year; start_year++) {
+                count += 365;
             }
-            if (start_day > end_day)
-            {
-                for(; start_month + 1 < end_month; start_month++)
-                {
+            if (start_day > end_day) {
+                for (; start_month + 1 < end_month; start_month++) {
                     count += g_month[start_month];
                 }
             }
-            else
-            {
-                for(; start_month < end_month; start_month++)
-                {
-                    count += g_month[start_month-1];
+            else {
+                for (; start_month < end_month; start_month++) {
+                    count += g_month[start_month - 1];
                 }
             }
         }
-        else if (start_month >= end_month)
-        {
-            for(; start_year < end_year; start_year++)
-            {
+        else if (start_month >= end_month) { // = else
+            for (; start_year < end_year; start_year++) {
                 count += 365;
             }
-            if (start_day > end_day)
-            {
-                for(; start_month + 1 > end_month; start_month--)
-                {
+            if (start_day > end_day) {
+                for (; start_month + 1 > end_month; start_month--) {
                     count -= g_month[start_month - 1];
                 }
             }
-            else
-            {
-                for (; start_month > end_month; start_month--)
-                {
+            else {
+                for (; start_month > end_month; start_month--) {
                     count -= g_month[start_month - 2];
                 }
             }
         }
-//        else if (start_month == end_month)
-//        {
-//            for(start_year; start_year < end_year; start_year++)
-//            {
-//                count += 365;
-//            }
-//            if (start_day > end_day)
-//            {
-//                for(start_month; start_month + 1 > end_month; start_month--)
-//                {
-//                    count -= g_month[start_month - 1];
-//                }
-//            }
-//            else
-//            {
-//                for (start_month; start_month > end_month; start_month--)
-//                {
-//                    count -= g_month[start_month - 2];
-//                }
-//            }
-//        }
     }
     else //start_year == end_year
     {
-        if(CheckLeapYear(start_year) && start_month<3 && end_month>=3)
-        {
+        if (CheckLeapYear(start_year) && start_month < 3 && end_month >= 3) {
             count++;
         }
-        if (start_day > end_day)
-        {
-            for(; start_month + 1 < end_month; start_month++)
-            {
+        if (start_day > end_day) {
+            for (; start_month + 1 < end_month; start_month++) {
                 count += g_month[start_month];
             }
         }
-        else
-        {
-            for(; start_month < end_month; start_month++)
-            {
-                count += g_month[start_month-1];
+        else {
+            for (; start_month < end_month; start_month++) {
+                count += g_month[start_month - 1];
             }
         }
     }
     return count;
 }
 
-bool CheckLeapYear (int year)
-{
-    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-    {
+bool CheckLeapYear (int year) {
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
         return true;
     }
-    else
-    {
+    else {
         return false;
     }
 }
--- C++/src/Algorithm/1331/main.cpp
+++ C++/src/Algorithm/1331/main.cpp
@@ -10,40 +10,34 @@ bool tester(const int cur[2], const int next[2], bool chess[6][6]);
 
 int main()
 {
-    bool chess[6][6];
-    bool flag(false);
+    bool chess[6][6]; //6*6 크기의 체스판 생성
     int error(0);
     int start[2], cur[2], next[2];
     string coordinate;
-    cin >> coordinate;
+
+    cin >> coordinate; //입력
 
     memset(chess, true, sizeof(chess));
-
     start[0] = coordinate[0] - 'A';
     start[1] = coordinate[1] - '1';
     cur[0] = start[0];
     cur[1] = start[1];
-//    chess[start[0]][start[1]] = false;
 
     for (int i = 0; i < 35; i++){
         cin >> coordinate;
-
         next[0] = coordinate[0] - 'A';
         next[1] = coordinate[1] - '1';
         if (tester(cur, next, chess)){
             cur[0] = next[0];
             cur[1] = next[1];
             chess[cur[0]][cur[1]] = false;
-            flag = true;
         }
         else {
             error++;
         }
     }
-    if(tester(cur, start, chess)){
-        flag = true;
-    }
-    else{
+
+    if(!tester(cur, start, chess)){
         error++;
     }
 
@@ -53,13 +47,14 @@ int main()
     else{
         cout << "Invalid" << endl;
     }
+
     return 0;
 }
 
 bool tester(const int cur[2], const int next[2], bool chess[6][6]){
     int x_movement = cur[0] - next[0];
     int y_movement = cur[1] - next[1];
-    bool flag = false;
+
     if(abs(x_movement) == 1 && abs(y_movement) == 2 && chess[next[0]][next[1]]){
         return true;
     }
