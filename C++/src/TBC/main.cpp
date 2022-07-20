6033577a4f227902b5820786b8fed7cc64d360d1
War-Oxi <xowl5460@naver.com>
2022-07-20 22:27:33 +0900

    MAC

--- C++/src/TBC/main.cpp
+++ C++/src/TBC/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
 int main(){
-    cout << ((true && true ) || false) << endl;
-    cout << true << endl;
-    cout << ((false && true) || true) << endl;
-    cout << true << endl;
-    cout << ((false && true) || false || true) << endl;
-    cout << true << endl;
-    cout << ((14 > 13 || 2 > 1) && (9 > 1)) << endl;
-    cout << true << endl;
-    cout << (!(2314123 > 2 || 123123 > 2387)) << endl;
-    cout << false << endl;
+    const unsigned char opt0 = 1 << 0;
+    const unsigned char opt1 = 1 << 1;
+    const unsigned char opt2 = 1 << 2;
+    const unsigned char opt3 = 1 << 3;
+
+    cout << bitset<8>(opt0) << endl;
+    cout << bitset<8>(opt1) << endl;
+    cout << bitset<8>(opt2) << endl;
+    cout << bitset<8>(opt3) << endl;
 
     return 0;
 }
