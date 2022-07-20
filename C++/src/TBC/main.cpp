417f89b0f77d2f32bc1d5fd3de615072ba27df5c
War-Oxi <xowl5460@naver.com>
2022-07-20 22:41:22 +0900

    TBC

--- C++/src/TBC/main.cpp
+++ C++/src/TBC/main.cpp
@@ -4,15 +4,33 @@
 using namespace std;
 
 int main(){
-    const unsigned char opt0 = 1 << 0;
-    const unsigned char opt1 = 1 << 1;
-    const unsigned char opt2 = 1 << 2;
-    const unsigned char opt3 = 1 << 3;
+    unsigned char option_viewed     = 0x01;
+    unsigned char option_edited     = 0x02;
+    unsigned char option_liked      = 0x04;
+    unsigned char option_shared     = 0x08;
+    unsigned char option_deleted    = 0x80;
 
-    cout << bitset<8>(opt0) << endl;
-    cout << bitset<8>(opt1) << endl;
-    cout << bitset<8>(opt2) << endl;
-    cout << bitset<8>(opt3) << endl;
+    unsigned char my_article_flags = 0;
+
+    cout << "option_viewed  => " <<std::bitset<8>(option_viewed) << endl;
+    cout << "option_edited  => " <<std::bitset<8>(option_edited) << endl;
+    cout << "option_liked   => " <<std::bitset<8>(option_liked) << endl;
+    cout << "option_shared  => " <<std::bitset<8>(option_shared) << endl;
+    cout << "option_deleted => " <<std::bitset<8>(option_deleted) << endl << endl;
+
+    my_article_flags |= option_viewed;
+    cout << "기사를 봤을 때" << endl << std::bitset<8>(my_article_flags) << endl << endl;
+
+    my_article_flags |= option_liked;
+    cout << "기사의 좋아요를 클릭했을 때 " << endl << std::bitset<8>(my_article_flags) << endl <<endl;
+
+    my_article_flags ^= option_liked;
+    cout << "기사의 좋아요를 다시 클릭햇을 때 " << endl << std::bitset<8>(my_article_flags) << endl << endl;
+
+    if(my_article_flags & option_viewed){
+        my_article_flags |= option_deleted;
+        cout << "본 기사만 삭제할 때 " << endl << std::bitset<8>(my_article_flags) << endl << endl;
+    }
 
     return 0;
 }
