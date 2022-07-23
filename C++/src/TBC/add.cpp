464b9ee498b0f1372c8e0e93334aa601e5479cbb
War-Oxi <xowl5460@naver.com>
2022-07-23 22:20:15 +0900

    extern declearation

--- C++/src/TBC/add.cpp
+++ C++/src/TBC/add.cpp
@@ -1,7 +1,7 @@
-//
-// Created by 김태지 on 2022/06/29.
-//
-int add(int a, int b)
-{
-    return a + b;
+#include <iostream>
+
+void doSomething(){
+    using namespace std;
+
+    cout << "Hello" << endl;
 }
