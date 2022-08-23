742e5552fd05081bd0c45195250cd5b118f1d646
War-Oxi <xowl5460@naver.com>
2022-08-23 19:52:58 +0900

    QUEUE

--- C++/src/Algorithm/1021_Spinning_Queue_[FAIL]/main.cpp
+++ C++/src/Algorithm/1021_Spinning_Queue_[FAIL]/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 //마지막 출력에서 실패함. 이유 모름
 int main() {
-    int queue_size(0), extract_num(0);
+    int queue_size(0), extract_num(0);//큐사이즈, 추출할 숫자의 개수
     cin >> queue_size >> extract_num;
-    int arr_extract_num[extract_num];
-    int queue[queue_size];
+    int arr_extract_num[extract_num];//추출할 숫자의 배열
+    int queue[queue_size];//큐
     for(int i=0; i<extract_num; i++){
         cin >> arr_extract_num[i];
     }
@@ -14,51 +14,56 @@ int main() {
         queue[i] = i+1;
     }
 
-    int cur_location(0);
-    int movement(0);
-    int tmp_movement[2];
+    int cur_location(0);//현재 인덱스
+    int movement(0);//총 이동 횟수
+    int tmp_movement[2];//더할 이동 횟수
 
-    int extract_frequency(0);
+    int extract_frequency(0);//현재 추출한 빈도
     while(extract_frequency < extract_num){
-        if(queue[cur_location] == arr_extract_num[extract_num]){
-            queue[cur_location] = -1;
+        tmp_movement[0]=0;
+        tmp_movement[1]=0;
+        if(queue[cur_location] == arr_extract_num[extract_frequency]){
+            cur_location++;
             cur_location = arr_extract_num[extract_num];
-            while(queue[cur_location+1] == -1){
+            while(queue[cur_location] == -1){
                 cur_location++;
                 if(cur_location == queue_size)
                     cur_location = 0;
             }
         }
         else{
-            tmp_movement[0]=0;
-            tmp_movement[1]=0;
             int tmp_cur_location = cur_location;
             while(queue[tmp_cur_location] != arr_extract_num[extract_frequency]){
                 tmp_cur_location++;
                 if(queue[tmp_cur_location] == -1)
                     continue;
+                tmp_movement[0]++;
                 if(tmp_cur_location == queue_size){
                     tmp_cur_location = 0;
                     while(queue[tmp_cur_location] == -1){
                         tmp_cur_location++;
+//                        if(queue[tmp_cur_location] != -1)
+//                            tmp_movement[0]--;
                     }
                 }
-                tmp_movement[0]++;
             }
             tmp_cur_location = cur_location;
             while(queue[tmp_cur_location] != arr_extract_num[extract_frequency]){
                 tmp_cur_location--;
                 if(queue[tmp_cur_location] == -1)
                     continue;
+                tmp_movement[1]++;
                 if(tmp_cur_location < 0){
                     tmp_cur_location = queue_size-1;
                     while(queue[tmp_cur_location] == -1){
                         tmp_cur_location--;
+//                        if(queue[tmp_cur_location] != -1)
+//                            tmp_movement[1]--;
                     }
                 }
-                tmp_movement[1]++;
             }
         }
+        queue[arr_extract_num[extract_frequency]-1] = -1;
         if(tmp_movement[0] <= tmp_movement[1])
             movement += tmp_movement[0];
         else
@@ -66,11 +71,14 @@ int main() {
         cur_location = arr_extract_num[extract_frequency];
         if(cur_location == queue_size){
             cur_location = 0;
-            while(queue[cur_location] == -1){
-                cur_location++;
-            }
+//            while(queue[cur_location] == -1){
+//                cur_location++;
+//            }
         }
-        queue[arr_extract_num[extract_frequency]-1] = -1;
+        while(queue[cur_location] == -1){
+            cur_location++;
+        }
+
         extract_frequency++;
     }
     cout << movement << endl;
