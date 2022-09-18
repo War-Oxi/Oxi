b16182bc970493f0399dc73f781a38860eeeca92
War-Oxi <xowl5460@naver.com>
2022-09-19 01:04:56 +0900

    11399_ATM, 11047_Coin_0

---
+++ C++/src/Algorithm/11399_ATM/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int num_of_people(0), min_time(0), waiting_time(0);
+    cin >> num_of_people;
+    int *arr_withdrawal_speed = new int [num_of_people];
+
+    for(int i=0; i<num_of_people; i++){
+        cin >> arr_withdrawal_speed[i];
+    }
+    sort(arr_withdrawal_speed, arr_withdrawal_speed+num_of_people);
+
+    for(int i=0; i<num_of_people; i++){
+        if(i == 0){
+            min_time += arr_withdrawal_speed[i];
+        }
+        else{
+            waiting_time += arr_withdrawal_speed[i-1];
+            min_time += waiting_time + arr_withdrawal_speed[i];
+        }
+    }
+
+    cout << min_time;
+    return 0;
+}
