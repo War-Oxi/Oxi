7e2c17ccacc62c31291b281e0248fd22862c5845
War-Oxi <xowl5460@naver.com>
2022-09-25 11:45:38 +0900

    DESKTOP_Algorithm

---
+++ Python_Code/Algorithm/Auto_lotto/main.py
@@ -0,0 +1,20 @@
+import random
+# 2018113009 김태지
+
+
+def Lotto_Generator(lotto_num):
+    for i in range(0, lotto_num):
+        list_lotto = list()
+        print("자동번호 --> ", end=' ')
+        for num in range(0, 6):
+            temp_num = random.randrange(1, 46)
+            while temp_num in list_lotto:
+                temp_num = random.randrange(1, 46)
+            list_lotto.append(temp_num)
+            print(list_lotto[num], end=' ')
+        print()
+
+
+print("** 로또 번호 생성을 시작합니다 **")
+input_lotto_num = int(input("몇 번을 뽑을까요? "))
+Lotto_Generator(input_lotto_num)
