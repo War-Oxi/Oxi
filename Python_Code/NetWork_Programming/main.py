360d17bb88568d78c48869140e01e478932352d0
War-Oxi <xowl5460@naver.com>
2022-10-13 03:24:27 +0900

    2022_10_13_MAC

--- Python_Code/NetWork_Programming/main.py
+++ Python_Code/NetWork_Programming/main.py
@@ -1,3 +1,9 @@
-a = 1.235
-a = round(a,2)
-print(a)
+def Prime_Num_Discriminator(num):
+    for i in range(2, int(num/2+1)):
+        if num % i == 0:
+            return
+    return num
+
+for i in range(2, 2000):
+    if(Prime_Num_Discriminator(i) != None):
+        print(Prime_Num_Discriminator(i))
