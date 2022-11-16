9238505760f4b8c23e86b5d86bf0072e7c4ec8a3
War-Oxi <xowl5460@naver.com>
2022-11-17 01:22:54 +0900

    2022_11_17_DESKTOP

--- Python_Code/NetWork_Programming/Chapter11_time_client.py
+++ Python_Code/NetWork_Programming/Chapter11_time_client.py
@@ -1,7 +1,6 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 3000))
+sock.connect(('localhost', 3300))
 print(sock.recv(1024).decode())
-# print(sock.recv(1024).decode())
 sock.close()
--- Python_Code/NetWork_Programming/Chapter11_time_server.py
+++ Python_Code/NetWork_Programming/Chapter11_time_server.py
@@ -1,7 +1,8 @@
 import socket
 import time
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ('', 3000)
+address = ('', 3300)
 sock.bind(address)
 sock.listen(5)
 
@@ -9,9 +10,8 @@ count = 1
 while True:
     client, addr = sock.accept()
     print("Connection requested from", addr)
-    print(addr)
-    print(type(addr))
-    client.send(f"현재시각 :{time.ctime(time.time())} \n당신은 {count}번째 접속자입니다.".encode())
+    client.send(f"현재시각 :{time.ctime(time.time())} "
+                f"\n당신은 {count}번째 접속자입니다.".encode())
     client.close()
     count += 1
 sock.close()
