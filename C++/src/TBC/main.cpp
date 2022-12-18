d17bcaa95b308f479ae12d3cbd35dae5cc8cc88b
War-Oxi <xowl5460@naver.com>
2022-12-19 03:53:04 +0900

    MAC

--- C++/src/TBC/main.cpp
+++ C++/src/TBC/main.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
+//#include <string>
+
 using namespace std;
 
-class IntArray{
+class Something{
+
 private:
-    int *m_arr = nullptr;
-    int m_arrSize = 0;
+    int     m_i;
+    double  m_d;
+    char    m_c;
 
 public:
-    explicit IntArray(const int sizeIn){
-        m_arrSize = sizeIn;
-        m_arr = new int[m_arrSize];
-
-        cout << "Constructor called" << endl;
+    Something()
+        : m_i(1.0)
+        , m_d{3.14}
+        , m_c('a')
+    {
+        m_i *= 2;
+        m_d *= 4;
+        m_c += 4;
     }
+//    {
+//        m_i = 1;
+//        m_d = 3.14;
+//        m_c = 'a';
+//    }
 
-    ~IntArray(){
-        if (m_arr != nullptr){
-            delete m_arr;
-            cout << "Destructor called" << endl;
-        }
-        else
-            cout << "Destructor not called" << endl;
+    void print(){
+        cout << m_i << " " << m_d << " " << m_c << endl;
     }
 };
 
 int main(){
-    for (int i =0; i<100; i++){
-        IntArray(1000);
-    }
+    Something a;
+    a.print();
 
     return 0;
 }
