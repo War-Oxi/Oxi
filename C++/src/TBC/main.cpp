63ebb3fb5201854a5d77682ab92df111cad1b63e
War-Oxi <xowl5460@naver.com>
2023-02-02 22:26:40 +0900

    MAC

--- C++/src/TBC/main.cpp
+++ C++/src/TBC/main.cpp
@@ -1,25 +1,89 @@
-#include "Monster.h"
-
-<<<<<<< HEAD
-int main(){
-    Monster myMonster("KangKang", Position2D(0,0));
-    myMonster.moveTo(Position2D(5, 9));
-    std::cout << myMonster << std::endl;
-
-    Monster myMonster2("Parsival", Position2D(0,0));
-
-    return 0;
-=======
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main()
+class Doctor; // forward declaration
+
+class Patient
 {
-    Monster mon1("Sanson", Position2D(0, 0));
-    cout << mon1 << endl;
-//    while (1)
+public:
+    string m_name;
+    vector<Doctor*> m_doctors;
+
+public:
+    Patient(string name_in)
+            : m_name(name_in)
+    {}
+
+    void addDoctor(Doctor * new_doctor)
     {
-        mon1.moveTo(Position2D(1, 1));
-        cout << mon1 << endl;
+        m_doctors.push_back(new_doctor);
     }
->>>>>>> 0d237e5f087aba07936aeae20b10fd0b7f9d7e06
+
+    void meetDoctors();
+
+    friend class Doctor;
+};
+
+class Doctor
+{
+private:
+    string m_name;
+    vector<Patient*> m_patients;
+
+public:
+    Doctor(string name_in)
+            : m_name(name_in)
+    {}
+
+    void addPatient(Patient * new_patient)
+    {
+        m_patients.push_back(new_patient);
+    }
+
+    void meetPatients()
+    {
+        for(auto & ele : m_patients)
+        {
+            cout << "Meet patient : " << ele->m_name << endl;
+        }
+    }
+
+    friend class Patient;
+};
+
+void Patient::meetDoctors()
+{
+    for (auto & ele : m_doctors)
+    {
+        cout << "Meet doctor : " << ele->m_name << endl;
+    }
+}
+int main(){
+    Patient *p1 = new Patient("Jack Jack");
+    Patient *p2 = new Patient("Dash");
+    Patient *p3 = new Patient("Violet");
+
+    Doctor *d1 = new Doctor("Doctor K");
+    Doctor *d2 = new Doctor("Doctor L");
+
+    p1->addDoctor(d1);
+    d1->addPatient(p1);
+
+    p2->addDoctor(d2);
+    d2->addPatient(p2);
+
+    p2->addDoctor(d1);
+    d1->addPatient(p2);
+
+    p1->meetDoctors();
+
+    d1->meetPatients();
+
+    delete p1;
+    delete p2;
+    delete p3;
+    delete d1;
+    delete d2;
 }
