48419bbba37bab8b46bccf33fefc7818c0950fec
War-Oxi <xowl5460@naver.com>
2023-02-07 01:36:19 +0900

    DESKTOP

--- C++/Algorithm/1874_StackSequence/main.cpp
+++ C++/Algorithm/1874_StackSequence/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
-#include
+#include <stack>
+#include <vector>
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    stack<int> stack1;
+    vector<char> result;
+
+    int cnt = 1;
+    int n;
+    cin >> n;
+
+    for (int i=0; i < n; i++){
+        int x;
+        cin >> x;
+
+        while ( cnt <= x ){
+            stack1.push(cnt++);
+            result.push_back('+');
+        }
+
+        if(stack1.top() == x) {
+            stack1.pop();
+            result.push_back('-');
+        }
+        else {
+            cout << "NO" << "\n";
+            return 0;
+        }
+    }
+
+    for(auto & element : result) {
+        cout << element << "\n";
+    }
     return 0;
 }
