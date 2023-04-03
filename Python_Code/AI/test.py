06515ff6222a27e45c09edc49ff3b251fb2df529
War-Oxi <xowl5460@naver.com>
2023-04-03 19:09:13 +0900

    MAC

--- Python_Code/AI/test.py
+++ Python_Code/AI/test.py
@@ -1,17 +1,11 @@
 from sklearn import datasets
 from sklearn import svm
+from sklearn.model_selection import cross_val_score
+import numpy as np
 
 digit = datasets.load_digits()
+s = svm.SVC(gamma=0.001)
+accuracies = cross_val_score(s, digit.data, digit.target, cv=5)
 
-s = svm.SVC(gamma=0.1, C=10)
-s.fit(digit.data, digit.target)
-
-new_d = [digit.data[0], digit.data[1], digit.data[2]]
-res = s.predict(new_d)
-print("예측값은", res)
-print("참값은", digit.target[0], digit.target[1], digit.target[2])
-
-res = s.predict(digit.data)
-correct=[i for i in range(len(res)) if res[i] == digit.target[i]]
-accuracy = len(correct)/len(res)
-print("화소 특징을 사용했을 때 정확률 =", accuracy*100, "%")
+print(accuracies)
+print("정확률(평균)=%0.3f, 표준편차=%0.3f" % (accuracies.mean() * 100, accuracies.std()))
