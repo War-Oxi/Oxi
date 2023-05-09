f0d959f1d62c41ea59dac5d02735e255affd37d0
War-Oxi <xowl5460@naver.com>
2023-05-09 22:51:39 +0900

    MAC

---
+++ C++/Algorithm/15683/cmake-build-debug/CMakeFiles/15683.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "15683"
+  "15683.pdb"
+  "CMakeFiles/15683.dir/main.cpp.o"
+  "CMakeFiles/15683.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15683.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
