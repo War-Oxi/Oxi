b850caef3b0437d97573f4ad2a024ce0732d7b6c
War-Oxi <xowl5460@naver.com>
2023-05-09 13:20:51 +0900

    MAC

--- C++/Algorithm_SW/1244/main.cpp
+++ C++/Algorithm_SW/1244/main.cpp
@@ -1,47 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
 
 int max_value;
+int sortedNum;
+int exchange_num;
+int min_change;
+string num;
 
-void init(vector<int> &vec_in);
-void get_max(vector<int> vec_in, int exchange_num);
+void dfs(int cnt){
+  int tmp_num = stoi(num);
+  if(cnt == exchange_num){
+    if(max_value < tmp_num) {
+      max_value = tmp_num;
+    }
+    return;
+  }
+
+  if(tmp_num == sortedNum){
+    min_change = cnt;
+    max_value = tmp_num;
+    return;
+  }
+
+  for(int i=cnt; i<num.length(); i++){
+    for(int j=i+1; j<num.length(); j++){
+      swap(num[i], num[j]);
+      dfs(cnt+1);
+      swap(num[i], num[j]);
+    }
+  }
+}
 
 int main() {
-    int test_case, T;
-    cin >> T;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    for(test_case=1; test_case<=T; test_case++){
-        vector<int> my_vec;
-        init(my_vec);
-        max_value = -1;
-        get_max(my_vec, 0);
+  int test_case, T;
+  cin >> T;
 
-
+  for(test_case=1; test_case<=T; test_case++){
+    cin >> num >> exchange_num;
+    string max_num = num;
+    sort(max_num.begin(), max_num.end(), greater<>());
+    sortedNum = stoi(max_num);
+    max_value = stoi(num);
+    dfs(0);
+    min_change = 10000000;
+    if(min_change < exchange_num && abs(min_change - exchange_num) % 2 != 0){
+      num = to_string(max_value);
+      if(num[0] != num[1])
+        swap(num[num.size()-1], num[num.size()-2]);
+      max_value = stoi(num);
     }
+    cout << "#" << test_case << " " << max_value << "\n";
+  }
 
-    return 0;
-}
-
-
-void init(vector<int> &vec_in){
-    string num;
-    cin >> num;
-
-
-    for(auto & ele : num){
-        vec_in.push_back(ele-48);
-    }
-}
-
-void get_max(vector<int> vec_in, int exchange_num) {
-    int max_exchange, exchange_count(0);
-    cin >> max_exchange;
-
-    for(int i=0; i<max_exchange; i++){
-
-        }
-    }
+  return 0;
 }
