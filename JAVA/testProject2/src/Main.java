89827ec263b2c2fc16108ef0011af05d44f4713d
Oxi <xowl5460@naver.com>
2023-06-25 23:47:14 +0900

    test

--- JAVA/testProject2/src/Main.java
+++ JAVA/testProject2/src/Main.java
@@ -1,17 +1,22 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList pitches;
+        pitches = new ArrayList();
+        pitches.add("138");
+        pitches.add("129");
+        pitches.add("142");
+        pitches.add(123);
+        pitches.add("안녕하세요");
+        pitches.add(true);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.print(pitches + "\n");
+        System.out.print(pitches.get(5).getClass().getName());
+
     }
 }
