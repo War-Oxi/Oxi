26fa87a1b2d3d3b98ab1c55d2f3834b4a696077e
War-Oxi <xowl5460@naver.com>
2023-08-05 04:26:01 +0900

    ToDoList Update

---
+++ Every-People/src/main/java/com/kkankkandev/everypeople/EveryPeople/EveryPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.kkankkandev.everypeople.EveryPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryPeopleApplication.class, args);
+	}
+
+}
