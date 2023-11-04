0ade0b3b141e271299943a7eb3a12b7661c6397e
War-Oxi <xowl5460@naver.com>
2023-11-04 13:24:02 +0900

    update

---
+++ JAVA/Generic/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		MyCustomList<String> list = new MyCustomList();
+		list.addElement("Element 1");
+		list.addElement("Element 2");
+		String value = list.get(0);
+
+
+		MyCustomList<Integer> list2 = new MyCustomList();
+		list2.addElement(5);
+		list2.addElement(7);
+		Integer number = list2.get(0);
+	}
+}
